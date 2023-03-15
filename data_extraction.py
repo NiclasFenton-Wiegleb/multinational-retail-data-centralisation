3c1c6b54ca0ff6754e15b513cd19ec5c50f65338
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-15 21:55:53 +0000

    Read_db_creds() function reads yaml file with credentials.

--- data_extraction.py
+++ data_extraction.py
@@ -1,1 +1,14 @@
+import yaml
+
 class DataExtractor:
+    def __init__(self) -> None:
+        pass
+
+    def read_db_creds(self, cred_file):
+        credentials = yaml.safe_load(cred_file)
+        return credentials
+    
+extractor = DataExtractor()
+file = open('db_creds.yaml', 'r')
+dic = extractor.read_db_creds(file)
+print(dic)
