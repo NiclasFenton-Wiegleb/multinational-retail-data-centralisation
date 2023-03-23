b7f6d7a584528806c7a23457d67ca2e703369a1a
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-23 21:17:45 +0000

    Successfully initialising db_engine.

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,5 @@
 import yaml
-from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 class DataExtractor:
     def __init__(self) -> None:
@@ -11,13 +11,25 @@ class DataExtractor:
             return credentials
         
     def init_db_engine(self, credentials):
-        db_engine = create_engine(credentials)
+        db_host = credentials["RDS_HOST"]
+        db_password = credentials["RDS_PASSWORD"]
+        db_usr = credentials["RDS_USER"]
+        db = credentials["RDS_DATABASE"]
+        db_port = credentials["RDS_PORT"]
+        db_engine = sqlalchemy.create_engine(url = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+            db_usr, db_password, db_host, db_port, db
+        ))
         return db_engine
+    
+    def list_db_tables(self, engine):
+        data = sqlalchemy.metadata.reflect(engine)
+        return data
 
     
 extractor = DataExtractor()
 file = 'db_creds.yaml'
 cred = extractor.read_db_creds(file)
 engine = extractor.init_db_engine(cred)
+tables = extractor.list_db_tables(engine)
 
 
