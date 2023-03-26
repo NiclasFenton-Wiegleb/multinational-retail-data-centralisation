94b1a0839274d12756183feb20173a5992809c92
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-26 13:42:26 +0200

    Rearranging columns.

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,6 +3,14 @@ import yaml
 import sqlalchemy
 import pandas as pd
 
+def tryconvert(value, default, *types):
+    for t in types:
+        try:
+            return t(value)
+        except (ValueError, TypeError):
+            continue
+    return default
+
 class DataCleaning:
 
     def __init__(self) -> None:
@@ -10,7 +18,13 @@ class DataCleaning:
 
     def clean_user_data(self, dataframe):
         df = dataframe.set_index("index")
+        df = df.reindex(columns= ["address", "longitude", "lat", "latitude", "locality", 
+                                  "country_code", "continent", "store_code", "store_type", 
+                                  "store_type", "staff_numbers", "opening_date"])
+        remove_non_numerics = lambda val : tryconvert(val, None, float)
         df["address"] = df["address"].astype("string")
+        df["longitude"] = df["longitude"].apply(remove_non_numerics)
+        df["latitude"] = df["latitude"].apply(remove_non_numerics)
         df["address"] = df["address"].str.replace('\n', ', ', regex= True)
         return df
 
@@ -32,3 +46,7 @@ clean_legacy_store_details = cleaner.clean_user_data(legacy_store_details)
 
 print(clean_legacy_store_details)
 print(clean_legacy_store_details.info())
+print(clean_legacy_store_details["localitiy"])
+
+# "address", "longitude", "lat", "latitude", "locality", "country_code", "continent", "store_code", 
+# "store_type", "store_type", "staff_numbers", "opening_date"
