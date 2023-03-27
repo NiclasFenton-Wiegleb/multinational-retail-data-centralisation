c38cb5fd1c4157e0a88c45f50c475d5f0c3f3291
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-27 09:15:04 +0200

    Changing column types from object to string and category.

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,14 +2,14 @@ import data_extraction
 import yaml
 import sqlalchemy
 import pandas as pd
+from datetime import datetime
 
-def tryconvert(value, default, *types):
+def try_convert_type(value, default, *types):
     for t in types:
         try:
             return t(value)
         except (ValueError, TypeError):
-            continue
-    return default
+            return default
 
 class DataCleaning:
 
@@ -21,7 +21,7 @@ class DataCleaning:
         df = df.reindex(columns= ["address", "longitude", "lat", "latitude", "locality", 
                                   "country_code", "continent", "store_code", "store_type", 
                                   "store_type", "staff_numbers", "opening_date"])
-        remove_non_numerics = lambda val : tryconvert(val, None, float)
+        remove_non_numerics = lambda val : try_convert_type(val, None, float)
         df = df.drop(df.index.get_loc(df.loc[df["lat"].notnull()]))
         df["address"] = df["address"].astype("string")
         df["longitude"] = df["longitude"].apply(remove_non_numerics)
@@ -31,6 +31,10 @@ class DataCleaning:
     
     def clean_user_data(self, dataframe):
         df = dataframe.set_index("index")
+        df[["first_name", "last_name", "company", "email_address"]] =  df[["first_name", 
+                                                                           "last_name", "company", 
+                                                                           "email_address"]].astype("string")
+        df[["country", "country_code"]] =  df[["country", "country_code"]].astype("category")
         return df
 
 extractor = data_extraction.DataExtractor()
@@ -49,6 +53,6 @@ orders_table = extractor.read_rds_table("orders_table")
 cleaner = DataCleaning()
 clean_user_data = cleaner.clean_user_data(legacy_users)
 
-print(clean_user_data)
+print(clean_user_data[["date_of_birth", "join_date"]])
 print(clean_user_data.info())
-#print(clean_legacy_store_details["localitiy"])
+
