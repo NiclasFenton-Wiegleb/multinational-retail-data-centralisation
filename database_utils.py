fb918d28c6a1972253c782c288ec128fe1a57bc1
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-29 18:32:50 +0200

    Code uploading user_data dataframe to database working outside of DatabaseConnector class.

--- database_utils.py
+++ database_utils.py
@@ -12,17 +12,17 @@ class DatabaseConnector:
     def upload_to_db(self, dataframe, table_name):
 
         #Connect to database
-        db_engine = sqlalchemy.create_engine(url = "postgresql://postgres:pass@localhost/Sales_Data")
-        db_connection = db_engine.connect()
+        db_engine = sqlalchemy.create_engine(url = "postgresql://postgres:Nick11375@localhost/Sales_Data")
 
         #Upload dataframe to database
         conn = psycopg2.connect(
             database= "Sales_Data",
             user= "postgres",
-            password= "pass",
+            password= "Nick11375",
             host= "localhost",
             port= "5432"
         )
+        
         conn.autocommit = True
         cursor = conn.cursor()
 
@@ -30,23 +30,24 @@ class DatabaseConnector:
         cursor.execute("drop table if exists user_data")
 
         sql = '''CREATE TABLE user_data(index int, first_name char(20), last_name char(20), date_of_birth date, company char(100), 
-        email_address char(70), address char(100), country char(20), country_code char(2), phone_number char(20), join_date date, user_uuid char(100));'''
+            email_address char(70), address char(100), country char(20), country_code char(2), phone_number char(20), join_date date, user_uuid char(100));'''
 
         cursor.execute(sql)
 
         #Convert dataframe to sql
-        df = dataframe
-        df.to_sql("user_data", con=db_engine, if_exists='replace')
-        cursor.execute("SELECT * FROM user_data").fetchall()
+        dataframe.to_sql("user_data", con=db_engine, if_exists='replace')
+        #cursor.execute("SELECT * FROM user_data").fetchall()
 
-        #Fetching all rows
-        #sql1 = '''SELECT * from user_data;'''
-       # cursor.execute(sql1)
-        #for i in cursor.fetchall():
-            #print(i)
 
-        conn.commit()
-        conn.close()
+            #Fetching all rows
+        sql1 = '''SELECT * from user_data;'''
+        cursor.execute(sql1)
+        for i in cursor.fetchall():
+            print(i)
+        cursor.commit()
+        cursor.close()
+        del cursor
+        
 
 #Extract data
 
@@ -65,6 +66,41 @@ clean_user_data = cleaner.clean_user_data(legacy_users)
 
 #Upload clean_user_data
 
-uploader = DatabaseConnector()
-data_upload = uploader.upload_to_db(clean_user_data, "user_data")
+#uploader = DatabaseConnector()
+#data_upload = uploader.upload_to_db(clean_user_data, "user_data")
 
+#Connect to database
+db_engine = sqlalchemy.create_engine(url = "postgresql://postgres:pass@localhost/Sales_Data")
+
+#Upload dataframe to database
+conn = psycopg2.connect(
+    database= "Sales_Data",
+    user= "postgres",
+    password= "pass",
+    host= "localhost",
+    port= "5432"
+)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+#Drop table if already exists
+cursor.execute("drop table if exists user_data")
+
+sql = '''CREATE TABLE user_data(index int, first_name char(20), last_name char(20), date_of_birth date, company char(100), 
+    email_address char(70), address char(100), country char(20), country_code char(2), phone_number char(20), join_date date, user_uuid char(100));'''
+
+cursor.execute(sql)
+
+#Convert dataframe to sql
+clean_user_data.to_sql("user_data", con=db_engine, if_exists='replace')
+#cursor.execute("SELECT * FROM user_data").fetchall()
+
+
+#Fetching all rows
+sql1 = '''SELECT * from user_data;'''
+cursor.execute(sql1)
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+del cursor
